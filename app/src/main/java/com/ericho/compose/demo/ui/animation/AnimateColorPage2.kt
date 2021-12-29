df4aebd1c84b5b007ea0765dfb797f350095489e
Eric Ho <wingsum.developer@gmail.com>
2021-12-29 18:09:38 +0800

    add infinite animation sample draft 2

--- app/src/main/java/com/ericho/compose/demo/ui/animation/AnimateColorPage2.kt
+++ app/src/main/java/com/ericho/compose/demo/ui/animation/AnimateColorPage2.kt
@@ -2,9 +2,11 @@ package com.ericho.compose.demo.ui.animation
 
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.border
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
@@ -27,12 +30,15 @@ fun AnimateColorPage2() {
     ) {
         //Part 1
         Text(text = "Infinite Animation")
+        val configuration = LocalConfiguration.current
+        val halfScreenWidth = (configuration.screenWidthDp / 2)
+        val maxHeight = 120 / 2
         val targetTimelineX = 3000
         val targetTimelineY = 1000
         val infiniteTransition = rememberInfiniteTransition()
 
         val offsetX by infiniteTransition.animateValue(
-            initialValue = -200, targetValue = 200,
+            initialValue = -halfScreenWidth, targetValue = halfScreenWidth,
             animationSpec = infiniteRepeatable(
                 animation = tween(targetTimelineX, easing = LinearEasing),
                 repeatMode = RepeatMode.Reverse
@@ -40,7 +46,7 @@ fun AnimateColorPage2() {
             typeConverter = Int.Companion.VectorConverter
         )
         val offsetY by infiniteTransition.animateValue(
-            initialValue = -200, targetValue = 200,
+            initialValue = -maxHeight, targetValue = maxHeight,
             animationSpec = infiniteRepeatable(
                 animation = tween(targetTimelineY, easing = LinearEasing),
                 repeatMode = RepeatMode.Reverse
@@ -59,6 +65,7 @@ fun AnimateColorPage2() {
             modifier = Modifier
                 .fillMaxWidth()
                 .height(120.dp)
+                .border(1.dp, MaterialTheme.colors.secondary)
         ) {
             Surface(
                 color = color1, shape = RoundedCornerShape(5.dp),
