ef44a46e4839715c77db8cdc4e925556bc42c4f0
Eric Ho <wingsum.developer@gmail.com>
2021-12-29 12:57:01 +0800

    add new example (animate color) 2

--- app/src/main/java/com/ericho/compose/demo/ui/animation/AnimateColorPage.kt
+++ app/src/main/java/com/ericho/compose/demo/ui/animation/AnimateColorPage.kt
@@ -1,8 +1,12 @@
 package com.ericho.compose.demo.ui.animation
 
 import androidx.compose.animation.Animatable
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateIntSize
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
@@ -12,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
 
@@ -98,6 +103,91 @@ fun AnimateColorPage() {
                 Text(text = "Other")
             }
         }
+        Spacer(modifier = Modifier.height(40.dp))
+        //Part 3
+        Text(text = "Animate to some color (updateTransition)")
+        var colorState: BoxState by remember {
+            mutableStateOf(BoxState.First)
+        }
+        val colorTransition = updateTransition(targetState = colorState, label = "transition")
+        val intermediateColor3 by colorTransition.animateColor(
+            transitionSpec = { tween(3000) },
+            label = "animateColor"
+        ) {
+            when (it) {
+                BoxState.First -> Color.Blue
+                BoxState.Second -> Color.Magenta
+                BoxState.Third -> Color.Yellow
+                BoxState.Fourth -> Color.Red
+                BoxState.Fifth -> Color.Green
+            }
+        }
+        val cornerRadius by colorTransition.animateDp(
+            transitionSpec = { tween(3000) },
+            label = "cornerRadius"
+        ) {
+            when (it) {
+                BoxState.First -> 5.dp
+                BoxState.Second -> 10.dp
+                BoxState.Third -> 15.dp
+                BoxState.Fourth -> 20.dp
+                BoxState.Fifth -> 25.dp
+            }
+        }
+        val colorSurfaceSize by colorTransition.animateIntSize(
+            transitionSpec = { tween(3000) },
+            label = "colorSurfaceSize"
+        ) {
+            when (it) {
+                BoxState.First -> IntSize(60, 60)
+                BoxState.Second -> IntSize(70, 70)
+                BoxState.Third -> IntSize(80, 80)
+                BoxState.Fourth -> IntSize(60, 100)
+                BoxState.Fifth -> IntSize(100, 60)
+            }
+        }
+//        val colorOffset by colorTransition.animateOffset(transitionSpec = { tween(3000)},
+//            label = "colorSurfaceSize"
+//        ) {
+//            when(it){
+//                BoxState.First-> Offset.Infinite
+//                BoxState.Second-> Offset(20f,20)
+//                BoxState.Third-> Offset.Zero
+//                BoxState.Fourth-> IntSize(60,100)
+//                BoxState.Fifth-> IntSize(100,60)
+//            }
+//        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+        ) {
+            Surface(
+                color = intermediateColor3, shape = RoundedCornerShape(cornerRadius),
+                modifier = Modifier
+                    .size(colorSurfaceSize.width.dp, colorSurfaceSize.height.dp)
+                    .align(Alignment.Center)
+            ) {
+
+            }
+        }
+        Row() {
+            Button(onClick = { colorState = BoxState.First }) {
+                Text(text = "First")
+            }
+            Button(onClick = { colorState = BoxState.Second }) {
+                Text(text = "Second")
+            }
+            Button(onClick = { colorState = BoxState.Third }) {
+                Text(text = "Third")
+            }
+            Button(onClick = { colorState = BoxState.Fourth }) {
+                Text(text = "Fourth")
+            }
+            Button(onClick = { colorState = BoxState.Fifth }) {
+                Text(text = "fifth")
+            }
+        }
     }
 }
 
@@ -105,4 +195,12 @@ fun AnimateColorPage() {
 @Composable
 fun AnimateColorPagePreview() {
     AnimateColorPage()
+}
+
+enum class BoxState {
+    First,
+    Second,
+    Third,
+    Fourth,
+    Fifth
 }
