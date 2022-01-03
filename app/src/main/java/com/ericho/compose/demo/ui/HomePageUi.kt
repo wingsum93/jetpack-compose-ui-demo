de18a4d4dfa7baef98732aab7a20f602d4cf3dbf
Eric Ho <wingsum.developer@gmail.com>
2022-01-03 12:53:03 +0800

    add temp draft

--- app/src/main/java/com/ericho/compose/demo/ui/HomePageUi.kt
+++ app/src/main/java/com/ericho/compose/demo/ui/HomePageUi.kt
@@ -1,11 +1,14 @@
 package com.ericho.compose.demo.ui
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ExperimentalGraphicsApi
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,10 +16,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.ericho.compose.demo.base.StandardButton
 import com.ericho.compose.demo.model.data2
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.layout.*
 
 @ExperimentalMaterialApi
 @ExperimentalAnimationApi
@@ -25,18 +26,46 @@ import androidx.compose.foundation.layout.padding
 fun HomePageUi(
     navHostController: NavHostController
 ) {
-    Column(
-        modifier = Modifier
-            .padding(8.dp)
-            .verticalScroll(rememberScrollState())
-    ) {
-        for (i in data2) {
-            StandardButton(text = i.title) {
-                navHostController.navigate(i.key)
+    // init data analyze
+    val allTags = data2.flatMap { it.tags }
+    allTags.sorted()
+    val scope = rememberCoroutineScope()
+    var filterTag by remember { mutableStateOf<String?>(null) }
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Ui Demo") }, backgroundColor = Color.White)
+        }
+    ) { paddingValue ->
+        val filteredList =
+            if (filterTag == null) data2 else data2.filter { it.tags.contains(filterTag) }
+        Column(
+            modifier = Modifier
+                .padding(paddingValue)
+                .padding(8.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Row(
+                modifier = Modifier.scrollable(rememberScrollState(), Orientation.Horizontal)
+            ) {
+                for (oneTag in allTags) {
+                    TextButton(onClick = {
+                        scope.launch {
+                            filterTag = oneTag
+                        }
+                    }) {
+                        Text(text = oneTag)
+                    }
+                }
             }
-            Spacer(modifier = Modifier.height(12.dp))
+            for (i in filteredList) {
+                StandardButton(text = i.title) {
+                    navHostController.navigate(i.key)
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+            }
         }
     }
+
 }
 
 @ExperimentalGraphicsApi
