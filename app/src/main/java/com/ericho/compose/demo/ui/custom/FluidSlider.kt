71156f94b908eab731b72e1eec5650bf3146eb77
Eric Ho <wingsum.developer@gmail.com>
2022-01-13 12:40:51 +0800

    fluidslider: adding temp draft

--- app/src/main/java/com/ericho/compose/demo/ui/custom/FluidSlider.kt
+++ app/src/main/java/com/ericho/compose/demo/ui/custom/FluidSlider.kt
@@ -3,8 +3,9 @@ package com.ericho.compose.demo.ui.custom
 import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -12,8 +13,12 @@ import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredHeightIn
 
 @Composable
 fun FluidSlider(
@@ -42,38 +47,77 @@ fun FluidSliderImpl(
     with(LocalDensity.current) {
         12.dp.toPx()
     }
-    Column(
-        modifier
-            .padding(10.dp, 50.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .requiredHeightIn(50.dp, 150.dp)
-        ) {
-            Canvas(modifier = Modifier
-                .fillMaxSize()
-                .pointerInput(start_text, end_text, duration) {
-                    detectHorizontalDragGestures { change, dragAmount ->
+    val BAR_CORNER_RADIUS = 2
+    val BAR_VERTICAL_OFFSET = 1.5f
+    val BAR_INNER_HORIZONTAL_OFFSET = 0
+    var position: Float by remember { mutableStateOf(0.5f) }
+    val barHeight: Dp = if (size == Size.NORMAL) 20.dp else 15.dp
+    val SLIDER_WIDTH = 4
+    val SLIDER_HEIGHT = 1 + BAR_VERTICAL_OFFSET
 
-                    }
-                }) {
-                drawRoundRect(color = bar_color)
-                //
-                val path = Path()
-                path.addArc(Rect(0f, 100f, 200f, 300f), 270f, 180f)
-                drawIntoCanvas {
-                    val paint = android.graphics.Paint()
-                    paint.textAlign = Paint.Align.CENTER
-                    paint.textSize = 64f
-                    paint.color = 0xffff0000.toInt()
-                    it.nativeCanvas.drawText("Hello", center.x, center.y, paint)
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .requiredHeightIn(50.dp, 150.dp)
+    ) {
+        Canvas(modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(start_text, end_text, duration) {
+                detectHorizontalDragGestures { change, dragAmount ->
+
                 }
+            }) {
+            val (canvasWidth, canvasHeight) = this.size
+            var zeroPoint = Offset.Zero
+            zeroPoint += Offset(10f, canvasHeight / 2)
+            canvasWidth
+            drawRoundRect(color = bar_color, topLeft = zeroPoint)
+            //
+            val path = Path()
+            path.addArc(Rect(0f, 100f, 200f, 300f), 270f, 180f)
+            drawIntoCanvas {
+                val paint = android.graphics.Paint()
+                paint.textAlign = Paint.Align.CENTER
+                paint.textSize = 64f
+                paint.color = 0xffff0000.toInt()
+                it.nativeCanvas.drawText("Hello", center.x, center.y, paint)
             }
         }
     }
+}
 
+object FluidSliderConstant {
+    const val BAR_CORNER_RADIUS = 2
+    const val BAR_VERTICAL_OFFSET = 1.5f
+    const val BAR_INNER_HORIZONTAL_OFFSET = 0
 
+    const val SLIDER_WIDTH = 4
+    const val SLIDER_HEIGHT = 1 + BAR_VERTICAL_OFFSET
+
+    const val TOP_CIRCLE_DIAMETER = 1
+    const val BOTTOM_CIRCLE_DIAMETER = 25.0f
+    const val TOUCH_CIRCLE_DIAMETER = 1
+    const val LABEL_CIRCLE_DIAMETER = 10
+
+    const val ANIMATION_DURATION = 400
+    const val TOP_SPREAD_FACTOR = 0.4f
+    const val BOTTOM_START_SPREAD_FACTOR = 0.25f
+    const val BOTTOM_END_SPREAD_FACTOR = 0.1f
+    const val METABALL_HANDLER_FACTOR = 2.4f
+    const val METABALL_MAX_DISTANCE = 15.0f
+    const val METABALL_RISE_DISTANCE = 1.1f
+
+    const val TEXT_SIZE = 12
+    const val TEXT_OFFSET = 8
+    const val TEXT_START = "0"
+    const val TEXT_END = "100"
+
+    const val COLOR_BAR = 0xff6168e7.toInt()
+    const val COLOR_LABEL = android.graphics.Color.WHITE
+    const val COLOR_LABEL_TEXT = android.graphics.Color.BLACK
+    const val COLOR_BAR_TEXT = android.graphics.Color.WHITE
+
+    const val INITIAL_POSITION = 0.5f
 }
 
 enum class Size(val value: Int) {
