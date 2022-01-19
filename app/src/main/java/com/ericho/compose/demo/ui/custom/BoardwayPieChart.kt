ec11b95305d0cb7639709771c521109eaa6ba503
Eric Ho <wingsum.developer@gmail.com>
2022-01-19 18:27:04 +0800

    pie chart: do draft 4

--- app/src/main/java/com/ericho/compose/demo/ui/custom/BoardwayPieChart.kt
+++ app/src/main/java/com/ericho/compose/demo/ui/custom/BoardwayPieChart.kt
@@ -3,6 +3,7 @@ package com.ericho.compose.demo.ui.custom
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -16,18 +17,33 @@ import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.ericho.compose.demo.R
 import com.ericho.compose.demo.ui.theme.JetpackComposeUiDemoTheme
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.sizeIn
 
 @Composable
 fun BoardwayPieChart(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    outlineProgressAngle: Float = 0f,
+    imageAngle: Float = 0f,
+    separatorAngle: Float = 5f,
+    circleOutlineColor: Color = Color(65, 236, 182, 255)
 ) {
+
+    val outlineSafeAngle =
+        remember(outlineProgressAngle) { outlineProgressAngle.coerceIn(0f..360f) }
+    val imageSafeAngle = remember(imageAngle) { imageAngle.coerceIn(0f..360f) }
+    val separatorSafeAngle = remember(separatorAngle) { separatorAngle.coerceIn(3f..20f) }
     val bitmap = ImageBitmap.imageResource(
         res = LocalContext.current.resources,
         id = R.drawable.boardway_shape
     )
+    //calculate sector of 3 arc
+    val isOutlineFull = outlineSafeAngle < 360f
+    val isImageFull = imageSafeAngle < 360f
+
     Canvas(
         modifier = modifier
+            .aspectRatio(1f)
             .sizeIn(
                 minWidth = 300.dp,
                 minHeight = 300.dp,
@@ -40,14 +56,13 @@ fun BoardwayPieChart(
             val heightInt = size.height.toInt()
             val widthInt = size.width.toInt()
             val spaceColor = Color.White
-            val circleOutlineColor = Color(65, 236, 182, 255)
             val angleOffset = -90f
             val angle1 = 0 + angleOffset
             val angle2 = 120 + angleOffset
             val angle3 = 240 + angleOffset
-            val separatorAngle = 5f
+
             val stokeWidth = size.width * 0.24f
-            val currentOutlineAngle = 200f
+            val currentOutlineAngle = outlineSafeAngle - imageSafeAngle
             ///
             drawImage(image = bitmap, srcSize = IntSize(widthInt, heightInt))
             // Head
@@ -68,26 +83,25 @@ fun BoardwayPieChart(
             //3 x space arc
             drawArc(
                 color = spaceColor,
-                startAngle = angle1 - separatorAngle / 2,
-                sweepAngle = separatorAngle,
+                startAngle = angle1 - separatorSafeAngle / 2,
+                sweepAngle = separatorSafeAngle,
                 useCenter = false,
                 style = Stroke(width = stokeWidth)
             )
             drawArc(
                 color = spaceColor,
-                startAngle = angle2 - separatorAngle / 2,
-                sweepAngle = separatorAngle,
+                startAngle = angle2 - separatorSafeAngle / 2,
+                sweepAngle = separatorSafeAngle,
                 useCenter = false,
                 style = Stroke(width = stokeWidth)
             )
             drawArc(
                 color = spaceColor,
-                startAngle = angle3 - separatorAngle / 2,
-                sweepAngle = separatorAngle,
+                startAngle = angle3 - separatorSafeAngle / 2,
+                sweepAngle = separatorSafeAngle,
                 useCenter = false,
                 style = Stroke(width = stokeWidth)
             )
-
         }
     )
 }
--- app/src/main/java/com/ericho/compose/demo/ui/custom/PieChatDemo.kt
+++ app/src/main/java/com/ericho/compose/demo/ui/custom/PieChatDemo.kt
@@ -1,25 +1,60 @@
 package com.ericho.compose.demo.ui.custom
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.*
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 
 @Composable
 fun PieChatDemo() {
-    Box(
+    ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp),
-        contentAlignment = Alignment.Center
+            .padding(10.dp)
     ) {
+        val (pieChart, btn) = createRefs()
+        var flowInProgress by remember { mutableStateOf(false) }
+        val transition = updateTransition(targetState = flowInProgress, "trans")
+        val outlineAngle by
+        transition.animateFloat(label = "pAngle", transitionSpec = { tween(2500) }) {
+            if (it) 360f else 0f
+        }
+        val pictureAngle by
+        transition.animateFloat(label = "pAngle", transitionSpec = { tween(3000, 2500) }) {
+            if (it) 360f else 0f
+        }
+
         BoardwayPieChart(
             Modifier
                 .fillMaxWidth(.9f)
                 .fillMaxHeight(.5f)
+                .constrainAs(pieChart) {
+                    centerTo(parent)
+                },
+            outlineProgressAngle = outlineAngle,
+            imageAngle = pictureAngle
         )
+
+        Button(
+            onClick = { flowInProgress = !flowInProgress },
+            modifier = Modifier.constrainAs(btn) {
+                top.linkTo(pieChart.bottom)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }) {
+            Text(text = "Click to toggle on / off")
+        }
     }
 }
 
